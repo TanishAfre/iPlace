a23c38ff0b2dab3e0e6c26f2433c5b494cca2b94
tanish <tanish.afre@gmail.com>
2024-09-27 19:55:17 +0100

    working fine

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 import StoreContextProvider from './context/StoreContext.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename='/iplace'>
   <StoreContextProvider>
   <App />
   </StoreContextProvider>
